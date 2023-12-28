5b0de58b4284d5d662b6902c6b013624dbb5462c
EstimeJeby <estimejeby78@gmail.com>
2023-12-28 15:15:37 -0500

    update finish

--- hep_english/settings.py
+++ hep_english/settings.py
@@ -46,7 +46,7 @@ SECRET_KEY= os.getenv("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG",False).lower() == "true"
 # DEBUG = True
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # ALLOWED_HOSTS = []
 
 # Application definition
