609d9e98a4966fb69563c653c7dcb8709d9064be
EstimeJeby <estimejeby78@gmail.com>
2024-01-08 13:08:50 -0500

    save update again css once one

--- hep_english/settings.py
+++ hep_english/settings.py
@@ -158,7 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATIC_ROOT = os.path.dirname(BASE_DIR, 'static/')
 
 # DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
 
